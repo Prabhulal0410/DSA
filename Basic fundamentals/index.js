3244cd38630c40a56537d677940079f591cd5520
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-02-09 21:33:45 +0530

    learning while loop

--- Basic fundamentals/index.js
+++ Basic fundamentals/index.js
@@ -1,3 +1,6 @@
+
+// For loop
+
 function sumArray(arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -9,3 +12,12 @@ console.log(sumArray([2,5,4]))
 
 
 
+// While loop
+
+let fruits = ["apple", "banana", "cherry"];
+let index = 0;
+
+while (index < fruits.length) {
+  console.log(fruits[index]);
+  index++;
+}
