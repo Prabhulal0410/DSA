dc0dcdd5a124cec33b1c048b625dba6113a8e6ff
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-02-23 21:38:48 +0530

    practice another problem

--- Basic fundamentals/patterns.js
+++ Basic fundamentals/patterns.js
@@ -21,12 +21,24 @@
 
 
 //right angle with num
+// let n = 4;
+
+// for (let i = 0; i <= n; i++) {
+//   let row = "";
+//   for (let j = 0; j <= i; j++) {
+//     row = row + (j + 1);
+//   }
+//   console.log(row);
+// }
+
+
+//right angle with num as per row
 let n = 4;
 
 for (let i = 0; i <= n; i++) {
   let row = "";
   for (let j = 0; j <= i; j++) {
-    row = row + (j + 1);
+    row = row + (i + 1);
   }
   console.log(row);
 }
