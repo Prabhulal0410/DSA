fc82c29d5993c3edc3da25c48ac553da79327320
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-02-24 21:32:01 +0530

    pratice inverted right angle pattern with num

--- Basic fundamentals/patterns.js
+++ Basic fundamentals/patterns.js
@@ -33,12 +33,24 @@
 
 
 //right angle with num as per row
-let n = 4;
+// let n = 4;
+
+// for (let i = 0; i <= n; i++) {
+//   let row = "";
+//   for (let j = 0; j <= i; j++) {
+//     row = row + (i + 1);
+//   }
+//   console.log(row);
+// }
+
+
+//inverted right angle with num
+let n = 5;
 
 for (let i = 0; i <= n; i++) {
   let row = "";
-  for (let j = 0; j <= i; j++) {
-    row = row + (i + 1);
+  for (let j = 0; j < n-i; j++) {
+    row = row + (j + 1);
   }
   console.log(row);
 }
