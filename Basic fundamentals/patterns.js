5511faf6557959e0f308bd20d2f120a8cfc38131
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-02-25 21:56:24 +0530

    pratice right angle probelm with 1010 pattern

--- Basic fundamentals/patterns.js
+++ Basic fundamentals/patterns.js
@@ -52,14 +52,29 @@
 //   console.log(row);
 // }
 
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   for (let j = 0; j < n - (i + 1); j++) {
+//     row = row + " ";
+//   }
+//   for (let k = 0; k < i + 1; k++) {
+//     row = row + "*";
+//   }
+//   console.log(row);
+// }
+
 let n = 5;
-for (let i = 0; i < n; i++) {
+for (let i = 0; i <= 5; i++) {
   let row = "";
-  for (let j = 0; j < n - (i + 1); j++) {
-    row = row + " ";
-  }
-  for (let k = 0; k < i + 1; k++) {
-    row = row + "*";
+  let toggle = 1;
+  for (let j = 0; j < i + 1; j++) {
+    row = row + toggle;
+    if (toggle == 1) {
+      toggle = 0;
+    } else {
+      toggle = 1;
+    }
   }
   console.log(row);
 }
