48dd7068cada66174e8eb5e4812597fdc0922d6e
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-02-25 22:00:15 +0530

    pratice right angle probelm with  pattern

--- Basic fundamentals/patterns.js
+++ Basic fundamentals/patterns.js
@@ -64,10 +64,26 @@
 //   console.log(row);
 // }
 
+// let n = 5;
+// for (let i = 0; i <= 5; i++) {
+//   let row = "";
+//   let toggle = 1;
+//   for (let j = 0; j < i + 1; j++) {
+//     row = row + toggle;
+//     if (toggle == 1) {
+//       toggle = 0;
+//     } else {
+//       toggle = 1;
+//     }
+//   }
+//   console.log(row);
+// }
+
+
 let n = 5;
+let toggle = 1;
 for (let i = 0; i <= 5; i++) {
   let row = "";
-  let toggle = 1;
   for (let j = 0; j < i + 1; j++) {
     row = row + toggle;
     if (toggle == 1) {
