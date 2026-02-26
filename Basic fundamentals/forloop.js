e98e54adf6e5d50d1363a02a8180acd974909a89
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-02-26 21:52:15 +0530

    solve num of count in digit number problem

--- Basic fundamentals/forloop.js
+++ Basic fundamentals/forloop.js
@@ -1,36 +1,36 @@
 // 🔹 Problem 1: Sum of Even Numbers
-let n = 10;   // change this number
-let total = 0;
+// let n = 10;   // change this number
+// let total = 0;
 
-for (let i = 1; i <= n; i++) {
-    if (i % 2 === 0) {
-        total += i;
-    }
-}
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 === 0) {
+//         total += i;
+//     }
+// }
 
-console.log("Sum of even numbers:", total);
+// console.log("Sum of even numbers:", total);
 
 
 // 🔹 Problem 2: Multiplication Table
-let num = 5;   // change this number
+// let num = 5;   // change this number
 
-for (let i = 1; i <= 10; i++) {
-    console.log(num + " x " + i + " = " + (num * i));
-}
+// for (let i = 1; i <= 10; i++) {
+//     console.log(num + " x " + i + " = " + (num * i));
+// }
 
 
 // 🔹 Problem 3: Count Vowels in a String
-let text = "hello world";   // change this text
-let count = 0;
+// let text = "hello world";   // change this text
+// let count = 0;
 
-for (let i = 0; i < text.length; i++) {
-    let ch = text[i].toLowerCase();
-    if ("aeiou".includes(ch)) {
-        count++;
-    }
-}
+// for (let i = 0; i < text.length; i++) {
+//     let ch = text[i].toLowerCase();
+//     if ("aeiou".includes(ch)) {
+//         count++;
+//     }
+// }
 
-console.log("Number of vowels:", count);
+// console.log("Number of vowels:", count);
 
 
 
@@ -54,17 +54,33 @@ console.log("Number of vowels:", count);
 
 
 // Printing Numbers Twice
-for (let i = 1; i <= 3; i++) {
-  for (let j = 1; j <= 2; j++) {
-    console.log("i =", i, "j =", j);
+// for (let i = 1; i <= 3; i++) {
+//   for (let j = 1; j <= 2; j++) {
+//     console.log("i =", i, "j =", j);
+//   }
+// }
+
+
+// for (let i = 1; i <= 2; i++) {
+//   console.log("Outer loop:", i);
+
+//   for (let j = 1; j <= 3; j++) {
+//     console.log("  Inner loop:", j);
+//   }
+// }
+
+
+
+//write a function that count of digits in a number
+function count(n){
+  let numCount = 0
+  while(n > 0){
+    n = Math.floor(n / 10)
+    numCount++
   }
+  return numCount
 }
 
-
-for (let i = 1; i <= 2; i++) {
-  console.log("Outer loop:", i);
-
-  for (let j = 1; j <= 3; j++) {
-    console.log("  Inner loop:", j);
-  }
-}
+let number = 213343556746;
+let result = count(number)
+console.log(result)
