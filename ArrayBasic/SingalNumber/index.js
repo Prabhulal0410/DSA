6e52608d7419de8e198c0a9525498be3eb0f22bf
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-03-24 21:37:42 +0530

    solve singal number problem

---
+++ ArrayBasic/SingalNumber/index.js
@@ -0,0 +1,19 @@
+// Method 1
+
+var singleNumber = function(nums) {
+    let hash = {}
+    for(let i = 0; i< nums.length; i++){
+        if(!hash[nums[i]]){
+             hash[nums[i]] = 1
+        }else{
+             hash[nums[i]] ++
+        }
+    }
+    for(let i = 0; i< nums.length; i++){
+        if(hash[nums[i]] == 1){
+            return nums[i]
+        }
+    }
+};
+
+console.log(singleNumber([2,5,2,6,5]))
