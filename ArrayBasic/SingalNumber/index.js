8d78dd300fa5df01ae29a060776dabfbb0aafa60
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-03-24 21:53:19 +0530

    solve singal number problem using bitwise xor

--- ArrayBasic/SingalNumber/index.js
+++ ArrayBasic/SingalNumber/index.js
@@ -1,19 +1,46 @@
 // Method 1
 
-var singleNumber = function(nums) {
-    let hash = {}
-    for(let i = 0; i< nums.length; i++){
-        if(!hash[nums[i]]){
-             hash[nums[i]] = 1
-        }else{
-             hash[nums[i]] ++
-        }
-    }
-    for(let i = 0; i< nums.length; i++){
-        if(hash[nums[i]] == 1){
-            return nums[i]
-        }
-    }
-};
+// var singleNumber = function(nums) {
+//     let hash = {}
+//     for(let i = 0; i< nums.length; i++){
+//         if(!hash[nums[i]]){
+//              hash[nums[i]] = 1
+//         }else{
+//              hash[nums[i]] ++
+//         }
+//     }
+//     for(let i = 0; i< nums.length; i++){
+//         if(hash[nums[i]] == 1){
+//             return nums[i]
+//         }
+//     }
+// };
 
-console.log(singleNumber([2,5,2,6,5]))
+// console.log(singleNumber([2,5,2,6,5]))
+
+
+
+// using ------> Bitwise xor
+
+var singleNumber = function(nums) {
+    let xor = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        xor = xor ^ nums[i]
+    }
+    return xor
+};
+console.log(singleNumber([2,5,2,6,5,6,9]))
+
+
+
+//Example how bitwise xor is wokrs
+// Bitwise XOR (exclusive OR) is an operation that compares two bits and returns:
+
+// 1 if the bits are different
+// 0 if the bits are the same
+
+let a = 5;  // 0101
+let b = 3;  // 0011
+
+console.log(a ^ b); // 6 (0110)
