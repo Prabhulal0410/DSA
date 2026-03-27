cf2933515b557e7e864a83f21233ec6cc06c03d7
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-03-27 22:15:05 +0530

    practice basic problem of recursion

--- Recursion/index.js
+++ Recursion/index.js
@@ -1,2 +1,15 @@
 
 // Defination --> Function call itself to solve smaller versions of the same problem
+
+// Two parts of recursion
+// 1 -> Base Case - Stop condition
+// 2 -> Recursive Case - part where function call itself
+
+function printNumbers(n) {
+    if (n > 5) return;   // base case (stop)
+
+    console.log(n);
+    printNumbers(n + 1); // recursive call
+}
+
+printNumbers(1);
