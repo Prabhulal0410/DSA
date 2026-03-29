069897633deb047bc68602c6ed92635afb6184a7
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-03-29 21:25:35 +0530

    practice recursion with array

--- Recursion/index.js
+++ Recursion/index.js
@@ -24,3 +24,14 @@ function factorial(n) {
 }
 
 console.log(factorial(5)); // 120
+
+
+
+function sumArray(arr) {
+  if (arr.length === 0) {
+    return 0; // base case
+  }
+  return arr[0] + sumArray(arr.slice(1)); // recursive case
+}
+
+console.log(sumArray([1, 2, 3, 4])); // 10
