3b5d38d5bf12f6365d6a82da9e692c73c4ce5cbb
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-04-05 22:15:03 +0530

    solve binary search problm

--- Searching & Sorting/BinarySearch/index.js
+++ Searching & Sorting/BinarySearch/index.js
@@ -1,3 +1,22 @@
 // we can apply binary search only on array which are sorted
 // divide array and try to find middle element
 // when we have to move our pointers or rwusde size of array we will wirte while loop
+// Time complexcity = o(log n) because we divide n/2 every time
+
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length-1
+    while(right >= left){
+        let middle = Math.floor((left + right)/2)
+
+        if(target === nums[middle]){
+            return middle
+        }else if(target < nums[middle]){
+            right = middle - 1
+        }else{
+            left = middle + 1
+        }
+    }
+    return -1
+};
+console.log(search([2,4,10,100,102],100))
