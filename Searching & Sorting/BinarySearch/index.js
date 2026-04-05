45dfe5a7c43bf42b61a469341211ec5508db755b
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-04-05 22:04:01 +0530

    started binary search

---
+++ Searching & Sorting/BinarySearch/index.js
@@ -0,0 +1,3 @@
+// we can apply binary search only on array which are sorted
+// divide array and try to find middle element
+// when we have to move our pointers or rwusde size of array we will wirte while loop
