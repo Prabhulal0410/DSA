08ec236c9bb04d9d207a111738102bacee018b6b
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-04-06 22:18:29 +0530

    learn bubble sort

---
+++ Searching & Sorting/BubbleSort/index.js
@@ -0,0 +1,15 @@
+function BubbleSort(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(BubbleSort([3, 6, 2, 1, 7, 0]));
