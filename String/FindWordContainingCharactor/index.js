93e184be8bc8e9527e6cc8c2a232544c7cabdcbf
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-04-13 21:22:15 +0530

    solve another string problem

---
+++ String/FindWordContainingCharactor/index.js
@@ -0,0 +1,26 @@
+var findWordsContaining = function(words, x) {
+    let res =[]
+    for(let i = 0; i < words.length; i++){
+        if(words[i].includes(x)){
+            res.push(i)
+        }
+    }
+    return res
+};
+console.log(findWordsContaining(["hello","hi"],"h"))
+
+
+
+var findWordsContaining = function(words, x) {
+    let res =[]
+    for(let i = 0; i < words.length; i++){
+        for(let j= 0; j < words[i].length;j++){
+            if(words[i][j]==x){
+                res.push(i)
+                break
+            }
+        }
+    }
+    return res
+};
+console.log(findWordsContaining(["hello","hi"],"h"))
