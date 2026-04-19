4d416d9c62a1a1ed21860be6d0eb33a89b830dce
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-04-19 21:54:38 +0530

    saved both approach

--- String/validPalindrom/index.js
+++ String/validPalindrom/index.js
@@ -1,36 +1,36 @@
-// brutforce approach
+// // brutforce approach
 
-var isPalindrome = function(s) {
-    s = s.toLowerCase();
-    let filterdString = "";
-    let reverseString = "";
-    for(let i = 0; i < s.length;i++){
-        if(s[i].match(/[a-z0-9]/i)){
-            filterdString += s[i]
-            reverseString = s[i] + reverseString
-        }   
-    }
-    return reverseString === filterdString
-};
+// var isPalindrome = function(s) {
+//     s = s.toLowerCase();
+//     let filterdString = "";
+//     let reverseString = "";
+//     for(let i = 0; i < s.length;i++){
+//         if(s[i].match(/[a-z0-9]/i)){
+//             filterdString += s[i]
+//             reverseString = s[i] + reverseString
+//         }   
+//     }
+//     return reverseString === filterdString
+// };
 
 
-// Approach 2
-var isPalindrome = function(s) {
-    s = s.toLowerCase();
-    let i = 0;
-    let j = s.length - 1;
+// // Approach 2
+// var isPalindrome = function(s) {
+//     s = s.toLowerCase();
+//     let i = 0;
+//     let j = s.length - 1;
 
-    while (i < j) {
-        if (!s[i].match(/[a-z0-9]/i)) {
-            ++i;
-        } else if (!s[j].match(/[a-z0-9]/i)) {
-            --j;
-        } else if (s[i] === s[j]) {
-            ++i;
-            --j;
-        } else {
-            return false;
-        }
-    }
-    return true;
-};
+//     while (i < j) {
+//         if (!s[i].match(/[a-z0-9]/i)) {
+//             ++i;
+//         } else if (!s[j].match(/[a-z0-9]/i)) {
+//             --j;
+//         } else if (s[i] === s[j]) {
+//             ++i;
+//             --j;
+//         } else {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
