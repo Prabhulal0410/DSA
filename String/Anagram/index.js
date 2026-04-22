3f671dcae0958b8fa9a5eff590148ecbd9e15464
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-04-22 22:12:09 +0530

    solved anagram problem

---
+++ String/Anagram/index.js
@@ -0,0 +1,17 @@
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false
+
+    let count = {}
+    for(let char of s){
+        count[char] = (count[char] || 0) + 1
+    }
+
+    for(let char of t){
+        if(!count[char] || count[char] < 0){
+            return false
+        }else{
+            -- count[char]
+        }
+    }
+    return true
+};
