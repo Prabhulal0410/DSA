660ea96d1ad05f185c9ee81c3c18b46d23077bf6
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-04-24 22:26:52 +0530

    solve isomorphic string problem

---
+++ String/IsomorphicString/index.js
@@ -0,0 +1,15 @@
+var isIsomorphic = function(s, t) {
+    let mapStoT = {};
+    let mapTtoS = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (!(s[i] in mapStoT) && !(t[i] in mapTtoS)) {
+            mapStoT[s[i]] = t[i];
+            mapTtoS[t[i]] = s[i];
+        } else if (mapStoT[s[i]] !== t[i] || mapTtoS[t[i]] !== s[i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
