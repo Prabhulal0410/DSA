5fbf205027671e8ea73e41f34555fb60645b318c
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-06-08 21:24:25 +0530

    practice reverseString problem

--- ArrayBasic/ReverseString/index.js
+++ ArrayBasic/ReverseString/index.js
@@ -1,11 +1,23 @@
-var reverseString = function(s) {
-    let len = s.length 
-    let halflen = Math.floor(len/2)
+// var reverseString = function(s) {
+//     let len = s.length 
+//     let halflen = Math.floor(len/2)
 
-    for(let i = 0; i < halflen ; i ++){
-        let temp = s[i]
-        s[i] = s[len-1- i]
-        s[len-1-i] = temp
-    }
-};
-console.log(reverseString(["h","e","l","l","o"]))
+//     for(let i = 0; i < halflen ; i ++){
+//         let temp = s[i]
+//         s[i] = s[len-1- i]
+//         s[len-1-i] = temp
+//     }
+// };
+// console.log(reverseString(["h","e","l","l","o"]))
+
+function reverseString(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+}
+
+console.log(reverseString("hello")); 
